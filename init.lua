8de5c0b4989f910e96a5a1b3eed2c1748881dec5
Shahab Dogar <shahab96@gmail.com>
2021-09-02 18:04:23 -0700

    Add language servers and configs

--- init.lua
+++ init.lua
@@ -4,22 +4,63 @@ vim.g.mapleader = ' '
 require('settings')
 require('plugins')
 require('keymappings')
+require('compe-config')
+
+-- config that activates keymaps and enables snippet support
+local function make_config()
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  capabilities.textDocument.completion.completionItem.snippetSupport = true
+  return {
+    -- enable snippet support
+    capabilities = capabilities,
+    -- map buffer local keybindings when the language server attaches
+    on_attach = on_attach,
+  }
+end
+
+-- Configure lua language server for neovim development
+local lua_settings = {
+  Lua = {
+    runtime = {
+      version = 'LuaJIT',
+      path = vim.split(package.path, ';'),
+    },
+    diagnostics = {
+      -- Get the language server to recognize the `vim` global
+      globals = { 'vim' },
+    },
+    workspace = {
+      -- Make the server aware of Neovim runtime files
+      library = {
+        [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+        [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+      }
+    },
+  }
+}
+
 
 local function setup_servers()
   require'lspinstall'.setup()
+
   local servers = require'lspinstall'.installed_servers()
+  table.insert(servers, 'lua')
+  table.insert(servers, 'python')
+  table.insert(servers, 'rust')
+  table.insert(servers, 'typescript')
+  table.insert(servers, 'json')
+  table.insert(servers, 'yaml')
+  table.insert(servers, 'graphql')
+
   for _, server in pairs(servers) do
-    require'lspconfig'[server].setup{}
+    local config = make_config()
+
+    if server == 'lua' then
+      config.settings = lua_settings
+    end
+
+    require'lspconfig'[server].setup(config)
   end
-  require'lspconfig'.lua.setup {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
-    }
-  }
 end
 
 setup_servers()
